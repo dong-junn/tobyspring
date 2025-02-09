d870e7ad7011972bb6c248bdcb30b64b085cb2e4
dong-junn <djl020326@gmail.com>
2025-02-10 07:36:10 +0900

    PaymentService 개발(2)

--- src/main/java/tobyspring/hellospring/Payment.java
+++ src/main/java/tobyspring/hellospring/Payment.java
@@ -1,4 +1,60 @@
 package tobyspring.hellospring;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Payment {
+
+    private Long orderId;
+    private String currency; // 통화 표시(USD, KRW, JPY)
+    private BigDecimal foreignCurrencyAmount; // 통화기준 금액
+    private BigDecimal exRate; // 적용환율
+    private BigDecimal convertedAmount; // 환율 계산된 금액
+    private LocalDateTime validUntil; // 환율 적용 유효기간
+
+    // 생성자의 매개변수의 타입이 다르므로 코드에 실수가 나올 수 있으며 이는 버그로 이어질 가능성이 있어 보인다 -> 배웠던 builder 패턴!!
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getForeignCurrencyAmount() {
+        return foreignCurrencyAmount;
+    }
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public LocalDateTime getValidUntil() {
+        return validUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "orderId=" + orderId +
+                ", currency='" + currency + '\'' +
+                ", foreignCurrencyAmount=" + foreignCurrencyAmount +
+                ", exRate=" + exRate +
+                ", convertedAmount=" + convertedAmount +
+                ", validUntil=" + validUntil +
+                '}';
+    }
 }
--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -1,14 +1,24 @@
 package tobyspring.hellospring;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class PaymentService {
 
-    public Payment prepare() {
-        return new Payment();
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+        /**
+         * [앞으로 구현해야 할 작업]
+         * 1. 환율 가져오기
+         * 2. 금액 계산
+         * 3. 유효시간 계산
+         */
+        return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO,
+                LocalDateTime.now());
     }
 
     public static void main(String[] args) {
         PaymentService paymentService = new PaymentService();
-        Payment payment = paymentService.prepare();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
 }
