1137a9c7ab7604b4c8f18b4d72b172e5a61026c6
dong-junn <djl020326@gmail.com>
2025-02-12 11:47:37 +0900

    annotation을 통한 bean 불러오기

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,11 +1,13 @@
 package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class ObjectFactory {
-
+/*
     @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
@@ -15,4 +17,5 @@ public class ObjectFactory {
     public ExRateProvider exRateProvider() {
         return new SimpleExRateProvider();
     }
+*/
 }
