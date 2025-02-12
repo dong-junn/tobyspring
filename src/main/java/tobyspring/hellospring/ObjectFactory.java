287ea7e82be85403159a0b1422913740d1f81e14
dong-junn <djl020326@gmail.com>
2025-02-12 12:06:50 +0900

    single-ton registry

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,21 +1,31 @@
 package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
 public class ObjectFactory {
-/*
+
     @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
 
     @Bean
+    public OrderService orderService() {
+        return new OrderService(exRateProvider());
+    }
+
+    @Bean
     public ExRateProvider exRateProvider() {
         return new SimpleExRateProvider();
     }
-*/
+}
+
+class OrderService {
+    ExRateProvider exRateProvider;
+
+    public OrderService(ExRateProvider exRateProvider) {
+        this.exRateProvider = exRateProvider;
+    }
 }
