0470f3f1b943c3fbeaf8ce3aa9ca80a2ee7b65b4
dong-junn <djl020326@gmail.com>
2025-02-13 21:06:56 +0900

    validate code test with spring container

---
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceSpringTest.java
@@ -0,0 +1,50 @@
+package tobyspring.hellospring.payment;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tobyspring.hellospring.TestObjectFactory;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import static java.math.BigDecimal.valueOf;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestObjectFactory.class)
+class PaymentServiceSpringTest {
+
+    @Autowired PaymentService paymentService;
+    @Autowired ExRateProviderStub exRateProviderStub;
+
+    @Test
+    void convertedAmount() throws IOException {
+
+        // exRateProviderStub에서 정한 기본값 1000 환율을 사용할 경우.
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        assertThat(payment.getExRate()).isEqualByComparingTo(valueOf(1_000));
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(valueOf(10_000));
+
+        // 근데 환율을 바꾸고 싶으면 어떻게 하지?
+        // exRateProviderStub에서 setter를 구현했기 때문에 setter로 값을 변경하면 된다
+
+        // exRate : 500으로 할 경우
+        exRateProviderStub.setExRate(valueOf(500));
+        Payment payment2 = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        assertThat(payment2.getExRate()).isEqualByComparingTo(valueOf(500));
+        assertThat(payment2.getConvertedAmount()).isEqualByComparingTo(valueOf(5_000));
+
+        //원화 환산 금액의 유효시간을 계산해야 함
+//        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+//        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+    }
+
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
+
+    }
+}
