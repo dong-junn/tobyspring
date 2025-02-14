5f463b73d1ed12ce89a9ce7f133b31bab6b8754c
dong-junn <djl020326@gmail.com>
2025-02-14 23:58:13 +0900

    WebApiExRateProvider : remove unused import statement

--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -5,14 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import tobyspring.hellospring.api.SimpleApiExecutor;
 import tobyspring.hellospring.payment.ExRateProvider;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.stream.Collectors;
 
 public class WebApiExRateProvider implements ExRateProvider {
 
