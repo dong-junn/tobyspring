91f902cd04b569c6e5211271a7f709b92c33f513
dong-junn <djl020326@gmail.com>
2025-02-16 19:13:34 +0900

    OrderRepository DIP

--- src/main/java/tobyspring/hellospring/order/OrderService.java
+++ src/main/java/tobyspring/hellospring/order/OrderService.java
@@ -3,18 +3,17 @@ package tobyspring.hellospring.order;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.support.TransactionTemplate;
-import tobyspring.hellospring.data.OrderRepository;
 
 import java.math.BigDecimal;
 
 @Service
 public class OrderService {
 
-    private final OrderRepository orderRepository;
+    private final OrderRepository jpaOrderRepository;
     private final JpaTransactionManager transactionManager;
 
-    public OrderService(OrderRepository orderRepository, JpaTransactionManager transactionManager) {
-        this.orderRepository = orderRepository;
+    public OrderService(OrderRepository jpaOrderRepository, JpaTransactionManager transactionManager) {
+        this.jpaOrderRepository = jpaOrderRepository;
         this.transactionManager = transactionManager;
     }
 
@@ -22,7 +21,7 @@ public class OrderService {
         Order order = new Order(no, total);
 
         return new TransactionTemplate(transactionManager).execute(status -> {
-            this.orderRepository.save(order);
+            this.jpaOrderRepository.save(order);
             return order;
         });
     }
