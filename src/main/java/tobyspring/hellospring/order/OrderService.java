8f6afb4628d052b42353fb523c64697b0bcd244e
dong-junn <djl020326@gmail.com>
2025-02-16 21:45:01 +0900

    transaction proxy

--- src/main/java/tobyspring/hellospring/order/OrderService.java
+++ src/main/java/tobyspring/hellospring/order/OrderService.java
@@ -1,33 +1,9 @@
 package tobyspring.hellospring.order;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.support.TransactionTemplate;
-
 import java.math.BigDecimal;
 import java.util.List;
 
-@Service
-public class OrderService {
-
-    private final OrderRepository jpaOrderRepository;
-    private final PlatformTransactionManager transactionManager;
-
-    public OrderService(OrderRepository jpaOrderRepository, PlatformTransactionManager transactionManager) {
-        this.jpaOrderRepository = jpaOrderRepository;
-        this.transactionManager = transactionManager;
-    }
-
-    public Order createOrder(String no, BigDecimal total) {
-        Order order = new Order(no, total);
-
-        this.jpaOrderRepository.save(order);
-        return order;
-    }
-
-    public List<Order> createOrders(List<OrderReq> reqs) {
-        return new TransactionTemplate(transactionManager).execute(status ->
-            reqs.stream().map(req -> createOrder(req.no(), req.total())).toList()
-        );
-    }
+public interface OrderService {
+    Order createOrder(String no, BigDecimal total);
+    List<Order> createOrders(List<OrderReq> reqs);
 }
