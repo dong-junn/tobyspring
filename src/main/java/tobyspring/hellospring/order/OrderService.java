38016fb56aa8c0ed26aa6b18de3fac66d09aee92
dong-junn <djl020326@gmail.com>
2025-02-16 20:26:11 +0900

    transaction test

--- src/main/java/tobyspring/hellospring/order/OrderService.java
+++ src/main/java/tobyspring/hellospring/order/OrderService.java
@@ -5,6 +5,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class OrderService {
@@ -20,9 +21,13 @@ public class OrderService {
     public Order createOrder(String no, BigDecimal total) {
         Order order = new Order(no, total);
 
-        return new TransactionTemplate(transactionManager).execute(status -> {
-            this.jpaOrderRepository.save(order);
-            return order;
-        });
+        this.jpaOrderRepository.save(order);
+        return order;
+    }
+
+    public List<Order> createOrders(List<OrderReq> reqs) {
+        return new TransactionTemplate(transactionManager).execute(status ->
+            reqs.stream().map(req -> createOrder(req.no(), req.total())).toList()
+        );
     }
 }
--- src/test/java/tobyspring/hellospring/order/OrderServiceSpringTest.java
+++ src/test/java/tobyspring/hellospring/order/OrderServiceSpringTest.java
@@ -4,12 +4,17 @@ package tobyspring.hellospring.order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tobyspring.hellospring.OrderConfig;
 
+import javax.sql.DataSource;
 import java.math.BigDecimal;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -20,10 +25,41 @@ public class OrderServiceSpringTest {
     @Autowired
     OrderService orderService;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     void createOrder() {
         var order = orderService.createOrder("0100", BigDecimal.ONE);
 
         assertThat(order.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    void createOrders() {
+        List<OrderReq> orderReqs = List.of(
+                new OrderReq("0200", BigDecimal.ONE),
+                new OrderReq("0201", BigDecimal.TWO)
+        );
+
+        var orders = orderService.createOrders(orderReqs);
+
+        assertThat(orders).hasSize(2);
+        orders.forEach(order -> assertThat(order.getId()).isGreaterThan(0));
+    }
+
+    @Test
+    void createDuplicatedOrders() {
+        List<OrderReq> orderReqs = List.of(
+                new OrderReq("0300", BigDecimal.ONE),
+                new OrderReq("0300", BigDecimal.TWO)
+        );
+
+        assertThatThrownBy(() -> orderService.createOrders(orderReqs))
+                .isInstanceOf(DataIntegrityViolationException.class);
+
+        JdbcClient client = JdbcClient.create(dataSource);
+        var count = client.sql("select count(*) from orders where no = '0300'").query(Long.class).single();
+        assertThat(count).isEqualTo(0);
     }
 }
